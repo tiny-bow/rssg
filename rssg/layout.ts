cf36ea4b43cae02f3c4f2657dd826f204a97b38c
nox <noxabellus@gmail.com>
2025-06-28 05:32:40 -0500

    oops 2.0

--- rssg/layout.ts
+++ rssg/layout.ts
@@ -44,7 +44,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(breadCrumbOpts), Component.ArticleTitle()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
